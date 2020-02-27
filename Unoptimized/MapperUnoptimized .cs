858e3eb1222c6a0f1cb8c0b15ce38af931f8f560
Zsolt Toth <gerzson63@gmail.com>
2020-02-27 18:08:13 +0100

    Retargeting

---
+++ Unoptimized/MapperUnoptimized .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace O2OMapper.Unoptimized
+{
+    public class MapperUnoptimized : ObjectCopyBase
+    {
+        public override void MapTypes(Type source, Type target)
+        {
+        }
+
+        public override void Copy(object source, object target)
+        {
+            var sourceType = source.GetType();
+            var targetType = target.GetType();
+            var propMap = GetMatchingProperties(sourceType, targetType);
+           
+            foreach (var prop in propMap)
+            {
+                var sourceValue = prop.SourceProperty.GetValue(source, null);
+                prop.TargetProperty.SetValue(target, sourceValue, null);
+            }
+        }
+    }
+}
